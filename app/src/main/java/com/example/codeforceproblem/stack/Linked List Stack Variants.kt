1f21a0706b5435c47a5c309313ce3bd397385f70
Md Eusuf Uddin <eusuf15-8926@diu.edu.bd>
2025-04-19 01:48:10 +0600

    one added

--- app/src/main/java/com/example/codeforceproblem/stack/Linked List Stack Variants.kt
+++ app/src/main/java/com/example/codeforceproblem/stack/Linked List Stack Variants.kt
@@ -2,4 +2,68 @@ package com.example.codeforceproblem.stack
 
 fun main(){
 
+    val size = 4
+    val stack = StackVariants(size)
+    var i = 1
+
+    while (i <= size){
+        println("Push Data $i")
+        stack.push(i)
+        i++
+    }
+
+    println("????????????????????????????????????")
+    stack.popElement()
+
+}
+
+class StackVariants(mSize: Int){
+
+    private val array = IntArray(mSize)
+    private var size: Int  = mSize
+    private var top = -1
+
+    fun popElement(){
+        if(top == -1){
+            println("Stack is underflow")
+            return
+        }
+        while (top != -1){
+            println("Pop Data ${array[top]}")
+            top--
+        }
+    }
+
+    fun push(element: Int){
+        if(top == size-1){
+            println("Stack is Overflow")
+            return
+        }
+        array[++top] = element
+    }
+
+    fun pop(){
+        if (top ==-1){
+            println("Stack is underflow")
+            return
+        }
+
+        array[top--]
+    }
+
+    fun peek(){
+        if(top == -1){
+            println("Stack Underflow")
+            return
+        }
+        array[top]
+    }
+
+    fun isEmpty(): Boolean{
+        return  top == -1
+    }
+
+    fun isFull(): Boolean{
+        return top == size-1
+    }
 }
